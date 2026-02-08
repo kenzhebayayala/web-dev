3aca3edde2dd87825b3d973dde5c69a1a4ffa178
kenzhebayayala <kenzhebayaiala@icloud.com>
2026-02-08 15:38:12 +0500

    Add files via upload

---
+++ lab3/jsFundaments/if.js
@@ -0,0 +1,17 @@
+
+
+let year = 1777;
+if (year == 2015)
+console.log( 'You are right!' );
+
+
+let Year = 2099;
+if (Year < 2015) {
+console.log( 'Too early...' );
+} else if (Year > 2015) {
+console.log( 'Too late' );
+} else {
+console.log( 'Exactly!' );
+}
+
+
