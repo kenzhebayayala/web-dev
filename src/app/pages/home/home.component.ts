f7abb799a15780f44aa03dafadc37f547f15f3e2
Ayala <kenzhebayaiala@icloud.com>
2026-03-04 12:28:00 +0500

    Delete src directory

--- src/app/pages/home/home.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
-
-@Component({
-  standalone: true,
-  imports: [RouterLink],
-  templateUrl: './home.component.html',
-})
-export class HomeComponent {}
